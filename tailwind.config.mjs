3f5013e72a6c33d2b3b4b336c363d93d642b0ff7
Mariano Bagnarelli <mbagnarelli.e@cocha.com>
2024-02-13 06:32:51 -0300

    tailwind installation & Hero component created

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+      fontFamily: {
+        sans: ['Inder', ...defaultTheme.fontFamily.sans],
+      },
+			colors: {
+				'lime': 'oklch(62.91% 0.1914 270)'
+			}
+		},
+	},
+	plugins: [
+],
+}
